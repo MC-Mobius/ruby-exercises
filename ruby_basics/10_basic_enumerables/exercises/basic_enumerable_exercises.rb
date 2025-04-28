0b81820b163da010661a2322bb14a2c896cabb4d
MC-Mobius <171867670+MC-Mobius@users.noreply.github.com>
2025-04-28 22:06:00 +0300

    Add Q2 first attempt

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -8,6 +8,7 @@ def display_guess_order(guesses)
   # use #each_with_index to iterate through each item of the guesses (an array)
   # use puts to output each list item "Guess #<number> is <item>" to console
   # hint: the number should start with 1
+  guesses.each_with_index(1) { |item, number| puts "Guess ##{number} is #{item}"}
 end
 
 def find_absolute_values(numbers)
